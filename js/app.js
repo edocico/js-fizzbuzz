b80aa62cf46dfae84ec0983dcaf4000f05a7198f
edocico <138378767+edocico@users.noreply.github.com>
2023-10-06 16:10:11 +0200

    add box generation in the loop

--- js/app.js
+++ js/app.js
@@ -5,13 +5,26 @@ console.dir(griDOMElement)
 
 for (let i = 0; i < 100; i++) {
     let n = i + 1
+    const boxDOMElement = document.createElement('div')
     if ((n % 3 === 0) && (n % 5 === 0)) {
         //console.log('FizzBuzz')
+        griDOMElement.append(boxDOMElement)
+        boxDOMElement.classList.add('box-red')
+        boxDOMElement.innerHTML = n
     } else if (n % 3 === 0) {
         //console.log('Fizz')
+        griDOMElement.append(boxDOMElement)
+        boxDOMElement.classList.add('box-green')
+        boxDOMElement.innerHTML = n
     } else if (n % 5 === 0) {
         //console.log('Buzz')
+        griDOMElement.append(boxDOMElement)
+        boxDOMElement.classList.add('box-yellow')
+        boxDOMElement.innerHTML = n
     } else {
         //console.log(n)
+        griDOMElement.append(boxDOMElement)
+        boxDOMElement.classList.add('box-blu')
+        boxDOMElement.innerHTML = n
     }
 }
