61382a31401d4d9c36afdea49fd35cdaa4c78640
edocico <138378767+edocico@users.noreply.github.com>
2023-10-06 15:51:46 +0200

    add box style

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 console.log('hello world')
 
+const griDOMElement = document.querySelector('.app-main')
+console.dir(griDOMElement)
+
 for (let i = 0; i < 100; i++) {
     let n = i + 1
     if ((n % 3 === 0) && (n % 5 === 0)) {
-        console.log('FizzBuzz')
+        //console.log('FizzBuzz')
     } else if (n % 3 === 0) {
-        console.log('Fizz')
+        //console.log('Fizz')
     } else if (n % 5 === 0) {
-        console.log('Buzz')
+        //console.log('Buzz')
     } else {
-        console.log(n)
+        //console.log(n)
     }
 }
